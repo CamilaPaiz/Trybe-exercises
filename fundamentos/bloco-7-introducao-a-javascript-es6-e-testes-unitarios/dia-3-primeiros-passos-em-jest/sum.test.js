12ff04e8aa15c4fce9f5e26e63a7ff5968aa0b30
CamilaPaiz <camilapaiz@hotmail.com>
2022-07-19 10:05:11 -0300

    arquivos dia 3-jest

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -0,0 +1,15 @@
+const sum = require('./sum');
+
+
+describe('Teste para validar a funcao sum',()=>{
+    it('Verifica sum(4,5) retorna 9',()=>{
+        expect(sum(4,5)).toBe(9);
+    })
+    it('Verifica sum(0,0) retorna 0',() =>{
+        expect(sum(0,0).toBe(0));
+    })
+    it('Dispara erro caso receba como parâmetro uma string',() =>{
+        expect(() =>sum(4,'5')).toThrowError();
+        expect(() =>sum(4,'5')).toThrowError('parâmetro tem que ser número');
+    })
+})
